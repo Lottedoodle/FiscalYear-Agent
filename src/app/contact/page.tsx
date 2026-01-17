55999df578b0735bda402f29894f6efa48c9eb7b
Lottedoodle <skydown1997@gmail.com>
2026-01-17 17:05:24 +0700

    ai chatbot mod

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 export default function Contact() {
   return (
     <>
-      <h1 className="text-4xl text-red-500">Contact page</h1>
+      <h1 className="text-xl text-red-500">Contact page</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
     </>
   )

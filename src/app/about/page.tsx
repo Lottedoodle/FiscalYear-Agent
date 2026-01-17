b7723321ddcfb0d791540c70ca0d671f31bc8e62
Lottedoodle <skydown1997@gmail.com>
2026-01-17 16:56:09 +0700

    ai-chatbot

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <>
+      <h1>About</h1>
+      <p className="text-4xl">About</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+    </>
+  )
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <>
+      <h1 className="text-4xl text-red-500">Contact page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+    </>
+  )
+}
